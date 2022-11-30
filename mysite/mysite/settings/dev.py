76cb89f224377d07f11eb4ef66b125a16dda08c2
Melvyn Wright <melwright61@virginmedia.com>
2022-11-30 17:09:09 +0000

    initial commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!9y*7pa-e(wn+(44r__vjnp(x@y#@v8hovipy=^rtb($v46o+%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
